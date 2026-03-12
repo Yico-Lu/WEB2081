48c34eaaa0194d531a2ee348af723ecb31acbea2
Tran Xuan Dung <xuandungg2601@gmail.com>
2026-03-12 12:54:13 +0700

    lab 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Contact } from '../pages/contact/contact';
+import { Home } from '../pages/home/home';
+import { About } from '../pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path:'',
+    component: Home,
+    },
+    {
+    path: 'contact',
+    component: Contact,
+    },
+    {
+    path: 'about',
+    component: About,
+    }
+];
---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css',
+})
+export class Contact {
+  Email = "xuandungg2601@gmail.com"
+}
